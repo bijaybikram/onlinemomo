8cafad2a464237fae82abdc22595c08525a1c51b
Bijay Bikram <becauseiam33@gmail.com>
2024-04-18 23:14:50 +0545

    API for User deletion implemented

--- routes/adminUserRoute.js
+++ routes/adminUserRoute.js
@@ -1,4 +1,7 @@
-const { getUsers } = require("../controller/admin/user/userController");
+const {
+  getUsers,
+  deleteUser,
+} = require("../controller/admin/user/userController");
 const isAuthenticated = require("../middleware/isAuthenticated");
 const catchAsync = require("../services/catchAsync");
 const restrictTo = require("../middleware/restrictTo");
@@ -8,4 +11,7 @@ router
   .route("/users")
   .get(isAuthenticated, restrictTo("admin"), catchAsync(getUsers));
 
+router
+  .route("/users/:id")
+  .delete(isAuthenticated, restrictTo("admin"), catchAsync(deleteUser));
 module.exports = router;
