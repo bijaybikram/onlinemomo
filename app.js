678ac54a703aba0c5a8f37fe1f394388ba6b219d
Bijay Bikram <becauseiam33@gmail.com>
2025-02-23 00:23:40 +0545

    websocket implemented for orderstatus updation

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const { connectDatabase } = require("./database/database");
 
-// const { Server } = require("socket.io");
+const { Server } = require("socket.io");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const promisify = require("util").promisify;
 
 // use DOTENV file
 
@@ -66,4 +68,46 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
   console.log(`Server started at PORT ${PORT}`);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3001", "http://localhost:5173"],
+    methods: ["*"], // Define allowed methods
+    credentials: true, // Allow credentials (if needed)
+  },
+});
+
+let onlineUsers = [];
+
+const addToOnlineUsers = (socketId, userId, role) => {
+  onlineUsers = onlineUsers.filter((user) => user.userId !== userId);
+  onlineUsers.push({ socketId, userId, role });
+  console.log("online Users:", onlineUsers);
+};
+
+io.on("connection", async (socket) => {
+  const { token } = socket.handshake.auth;
+  if (token) {
+    try {
+      const decryptedResult = await promisify(jwt.verify)(
+        token,
+        process.env.SECRET_KEY
+      );
+
+      //   check is that user exist on the field or not
+      const userExist = await User.findById(decryptedResult.id);
+
+      if (userExist) {
+        addToOnlineUsers(socket.id, userExist.id, userExist.userRole);
+      }
+    } catch (error) {
+      console.log("Token Verification Failed", error);
+    }
+  }
+
+  socket.on("updateOrderStatus", ({ status, orderId, userId }) => {
+    const foundUser = onlineUsers.find((user) => user.userId == userId);
+    io.to(foundUser.socketId).emit("statusUpdated", { status, orderId });
+  });
 });
